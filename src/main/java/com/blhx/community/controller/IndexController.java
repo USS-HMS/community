a5e8b1b49b75893448387ee92fb877992a7d14ce
BLHX <2819208060@qq.com>
2020-03-20 23:27:44 +0800

    add index and bootstrap

--- src/main/java/com/blhx/community/controller/IndexController.java
+++ src/main/java/com/blhx/community/controller/IndexController.java
@@ -6,10 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class HelloController {
+public class IndexController {
     @GetMapping("/")
-    public String hello( @RequestParam("name") String name , Model model ){
-        model.addAttribute("name",name);
-        return "hello";
+    public String hello( ){
+        return "index";
     }
 }
